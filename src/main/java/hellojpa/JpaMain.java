9d7b7dd724bd21b6556a5227d32d28c6939024b6
DowonJang <rosewoodowon@gmail.com>
2022-07-31 22:25:25 +0900

    1. 회원 조회, 수정, JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,11 +16,31 @@ public class JpaMain {
         tx.begin();
 
         try {
+            /*//생성
             Member member = new Member();
             member.setId(3L);
             member.setName("HelloC");
 
-            em.persist(member);
+            em.persist(member);*/
+
+            /*//조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() : " + findMember.getId());
+            System.out.println("findMember.getName() : " + findMember.getName());*/
+
+            /*//수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("HelloJPA");*/
+
+            //JPQL
+            List<Member> members = em.createQuery("SELECT m FROM Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : members) {
+                System.out.println("member : " + member.getName() );
+            }
 
             tx.commit();
         } catch (Exception e) {
